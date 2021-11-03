406a133ab0212c15f15b8bc5f5b41ff43a1023fc
nhienth <nhienth039@gmail.com>
2021-11-03 14:54:33 +0700

    updated code

---
+++ content/js/app.js
@@ -0,0 +1,27 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    // mybutton.style.display = "block";
+
+    mybutton.style.opacity = "1";
+    mybutton.style.visibility = "visible";
+  } else {
+    // mybutton.style.display = "none";
+    mybutton.style.opacity = "0";
+    mybutton.style.visibility = "hidden";
+    mybutton.style.transition = "all 0.2s ease";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
