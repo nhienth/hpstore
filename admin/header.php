aac9de21e675fe39b5f9ba86dbd6714c7df717e8
nhienth <nhienth039@gmail.com>
2021-11-09 17:11:00 +0700

    upload

--- admin/header.php
+++ admin/header.php
@@ -1,48 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADMIN</title>
-    <link rel="stylesheet" href="cssadmin.css">
-</head>
-<body>
-    <div class="controlll">
-    <div class="head1">
-        <div>
-            <img src="..//content/images/home/logo.png" alt="" width="230px" height="60px">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="icon"
+      href="//bizweb.dktcdn.net/100/180/515/themes/683553/assets/favicon.png?1624423469475"
+      type="image/x-icon"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <script
+      src="https://kit.fontawesome.com/497e58feb8.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="../content/css/reset.css">
+    <link rel="stylesheet" href="../content/css/app.css">
+    <link rel="stylesheet" href="../content/css/main.css">
+    <link rel="stylesheet" href="../content/css/admin.css">
+    <title>HPstore - Quản trị website</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="admin-layout">
+        <div class="layout-left">
+          <div class="left-logo">
+            <img src="../content/images/home/logo.png" alt="">
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--content">
+              <a href="index.php"><i class="fas fa-tv"></i> Trang chủ admin</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý tài khoản</div>
+            <div class="left-element--content">
+              <div class="more-function">
+                <a href="#">Thêm tài khoản</a>
+                <a href="#">Danh sách</a>
+              </div>
+              <a href=""><i class="fas fa-user"></i> Tài khoản</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý danh mục</div>
+            <div class="left-element--content">
+              <div class="more-function">
+                <a href="#">Thêm danh mục</a>
+                <a href="#">Danh sách</a>
+              </div>
+              <a href=""><i class="fas fa-copyright"></i> Danh mục</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý sản phẩm</div>
+            <div class="left-element--content">
+              <div class="more-function">
+                <a href="#">Thêm sản phẩm</a>
+                <a href="index.php?act=listhh">Danh sách</a>
+              </div>
+              <a href="index.php?act=listhh"><i class="fas fa-volleyball-ball"></i> Sản phẩm</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý đơn hàng</div>
+            <div class="left-element--content">
+              <a href=""><i class="fas fa-people-carry"></i> Đơn hàng</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý bình luận</div>
+            <div class="left-element--content">
+              <a href=""><i class="fas fa-comment-alt"></i> Bình luận</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý tin tức</div>
+            <div class="left-element--content">
+              <div class="more-function">
+                <a href="#">Thêm tin tức</a>
+                <a href="#">Danh sách tin tức</a>
+              </div>
+              <a href=""><i class="fas fa-newspaper"></i> Tin tức</a>
+            </div>
+          </div>
+          <div class="left-emelent">
+            <div class="left-element--title">Quản lý thống kê</div>
+            <div class="left-element--content">
+              <a href=""><i class="fas fa-chart-pie"></i> Thống kê</a>
+            </div>
+          </div>
         </div>
-        <div><h3>ADMIN - TRANG QUẢN TRỊ</h3></div>
-        
-        <div id="icon1">
-            <a href=""><i class="bi bi-house"></i><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-              </svg> TRANG WEB</a>
-        </div>
-    </div>
-    <div class="nav1">
-        <ul>
-            <li>
-                <a href="index.php?act=adddm">DANH MỤC |</a>
-            </li>
-            <li>
-                <a href="index.php?act=addhh">HÀNG HÓA |</a>
-            </li>
-            <li>
-                <a href="index.php?act=listkh">KHÁCH HÀNG |</a>
-            </li>
-            <li>
-                <a href="index.php?act=listhd">HÓA ĐƠN |</a>
-            </li>
-            <li>
-                <a href="index.php?act=thongke">THỐNG KÊ |</a>
-            </li>
-            <li>
-                <a href="#">TIN TỨC |</a>
-            </li>
-            <li>
-                <a href="index.php?act=listbl">BÌNH LUẬN</a>
-            </li>
-        </ul>
-    </div>
--- admin/home.php
+++ admin/home.php
@@ -1,1 +1,76 @@
-<h2>welcome to admin</h2>
+<div class="layout-right">
+          <div class="right-top">
+            <div class="website-title">Trang quản trị websitie</div>
+            <div class="admin-info">
+              <div class="admin-name">ADMIN</div>
+              <div class="admin-img">
+                <img src="../content/images/home/support-online.jpg" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="right-bgc">
+            <div class="right-main">
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Tài khoản</div>
+                  <div class="boxed-content--count">3</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-user-circle"></i>
+                </div>
+              </div>
+
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Danh mục</div>
+                  <div class="boxed-content--count">5</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-copyright"></i>
+                </div>
+              </div>
+
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Sản phẩm</div>
+                  <div class="boxed-content--count">25</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-volleyball-ball"></i>
+                </div>
+              </div>
+
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Đơn hàng</div>
+                  <div class="boxed-content--count">11</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-people-carry"></i>
+                </div>
+              </div>
+
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Tin tức</div>
+                  <div class="boxed-content--count">5</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-newspaper"></i>
+                </div>
+              </div>
+
+              <div class="right-main--boxed">
+                <div class="main-boxed--content">
+                  <div class="boxed-content--txt">Bình luận</div>
+                  <div class="boxed-content--count">10</div>
+                </div>
+                <div class="main-boxed--icon">
+                  <i class="fas fa-comment-alt"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
