b9a448dc45ee930c80f9110f1dd7c357544a8672
nhienth <nhienth039@gmail.com>
2021-11-11 22:33:49 +0700

    updated

--- admin/index.php
+++ admin/index.php
@@ -12,14 +12,6 @@
                 if(isset($_POST['themmoi'])){
                     $ten_danhmuc = $_POST['tenloai'];
                     if($ten_danhmuc == ""){
-<<<<<<< HEAD
-                        $thongbao2 = "Vui lòng nhập tên loại hàng";
-                        echo $thongbao2;
-                    }else{
-                        insert_danhmuc($ten_danhmuc);
-                        $thongbao2 = "THÊM THÀNH CÔNG";
-                        echo $thongbao2;
-=======
                         $thongbao = "Vui lòng nhập tên loại hàng";
                     }else if(exist_danhmuc($ten_danhmuc)){
                         $thongbao = "Danh mục đã tồn tại !";
@@ -27,7 +19,7 @@
                         insert_danhmuc($ten_danhmuc);
                         $thongbao = "Thêm danh mục thành công !";
                         $ten_danhmuc = "";
->>>>>>> 29991c1cffb8ed7e62373a7c25c58f4cfb1a450f
+                        
                     }
                 }
                 include "danhmuc/add.php";
@@ -270,33 +262,8 @@
             case 'listbl' :
                 include "binhluan/list.php";
                 break;
-<<<<<<< HEAD
-            case 'addkh' :
-                if(isset($_POST['themmoi']) && ($_POST['themmoi'])){
-                    $ten_dangnhap = $_POST['user'];
-                    $mat_khau = $_POST['pass'];
-                    $email = $_POST['email'];
-                    $so_dt = $_POST['sdt'];
-                    $ho_ten = $_POST['fullname'];
-                    $dia_chi = $_POST['address'];
-                    $hinh = $_FILES['file']['name'];
-                    $linkup = "img/";
-                    move_uploaded_file($_FILES['file']['tmp_name'],$linkup,$hinh);
-                    insert_khachhang($ten_dangnhap,$mat_khau,$ho_ten,$hinh,$so_dt,$email,$dia_chi);
-                    $thongbao = "Thêm thành công";
-                    echo $thongbao;
-                }
-                include "khachhang/add.php";
-                break;
-            case 'listtt' :
-                include "tintuc/list.php";
-                break;
-            case 'addtt' :
-                include "tintuc/add.php";
-                break;
-=======
             
->>>>>>> 29991c1cffb8ed7e62373a7c25c58f4cfb1a450f
+                
             default :
                 include "home.php";
                 break;
